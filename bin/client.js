181f685f0e075dac57295674846d7188bfd44921
Roman V. Liskovenko <rliskovenko@gmail.com>
2017-07-18 17:16:44 +0300

    Single port version;

--- bin/client.js
+++ bin/client.js
@@ -2,14 +2,15 @@ const net   = require( 'net' );
 const yargs = require( 'yargs' ).argv;
 
 var params = {
-  connect   : yargs.c || '127.0.0.1',
-  startPort : yargs.p || 60000,
-  portCount : yargs.n || 1000
+  connect    : yargs.c || '127.0.0.1',
+  startPort  : yargs.p || 60000,
+  portCount  : yargs.n || 1000,
+  singlePort : !!yargs.s
 };
 
 for ( i = params.startPort; i <= params.startPort + params.portCount; i++ ) {
   var s = new net.Socket;
-  s.connect( i, params.connect, function () {
+  s.connect( params.singlePort ? params.startPort : i, params.connect, function () {
     var socket = this;
     setInterval( function () {
       socket.write( socket.localPort.toString() );
--- bin/server.js
+++ bin/server.js
@@ -2,23 +2,39 @@ const net   = require( 'net' );
 const yargs = require( 'yargs' ).argv;
 
 var params = {
-  listen    : yargs.l || '127.0.0.1',
-  startPort : yargs.p || 60000,
-  portCount : yargs.n || 1000
+  listen     : yargs.l || '127.0.0.1',
+  startPort  : yargs.p || 60000,
+  portCount  : yargs.n || 1000,
+  singlePort : !!yargs.s
 };
 
-for ( i = params.startPort; i <= params.startPort + params.portCount; i++ ) {
+if ( params.singlePort ) {
   var s = new net.createServer( function ( s ) {
-    var port = s.localPort;
     s.on( 'close', function () {
-      console.log( "Disconnected from " + port );
+      console.log( "Disconnected" );
     } )
   } );
 
-  s.on( 'connection', function ( socket ) { } );
   s.on( 'error', function ( err ) {
     console.error( err );
   } );
 
-  s.listen( i, params.listen );
+  s.listen( params.startPort, params.listen );
+} else {
+  for ( i = params.startPort; i <= params.startPort + params.portCount; i++ ) {
+    var s = new net.createServer( function ( s ) {
+      var port = s.localPort;
+      s.on( 'close', function () {
+        console.log( "Disconnected from " + port );
+      } )
+    } );
+
+    s.on( 'connection', function ( socket ) { } );
+    s.on( 'error', function ( err ) {
+      console.error( err );
+    } );
+
+    s.listen( i, params.listen );
+  }
+
 }
